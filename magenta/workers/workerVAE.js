c916b2bcff9173a5e357159eb8eea9053a4cf560
HenriAugusto <msndohenri@hotmail.com>
2021-08-01 03:44:41 -0300

    Adapt workerVAE.js to use new model initialization system

--- magenta/workers/workerVAE.js
+++ magenta/workers/workerVAE.js
@@ -1,4 +1,3 @@
-
 /**
  * We need to use a TFJS version < 3.0.0
  * see https://github.com/magenta/magenta-js/issues/578
@@ -15,24 +14,23 @@ importScripts("/magenta/workers/workerUtils.js");
  * A medium-sized 4-bar, 90-class onehot melody model. Quantized to 2-byte weights.
  */
 music_vae_mel_4bar_q2 = new music_vae.MusicVAE("https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_med_q2");
-music_vae_mel_4bar_q2.initialize();
 
 /**
  * A 2-bar, 90-class onehot melody model with chord conditioning. Quantized to 2-byte weights.
  */
-//music_vae_2bar_chords = new music_vae.MusicVAE("https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_chords");
+music_vae_2bar_chords = new music_vae.MusicVAE("https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_chords");
 //music_vae_2bar_chords.initialize();
 
 /**
  * A 2-bar, 90-class onehot melody model. Less accurate, but smaller in size than full model.
  */
-//music_vae_2bar_small = new music_vae.MusicVAE("https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small");
+music_vae_2bar_small = new music_vae.MusicVAE("https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small");
 //music_vae_2bar_small.initialize();
 
 /**
  * A 2-bar, 9-class multilabel drum model with a NADE decoder. Quantized to 2-byte weights.
  */
-//drums_vae_2bar_nade_9_q2 = new music_vae.MusicVAE("https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/drums_2bar_nade_9_q2");
+drums_vae_2bar_nade_9_q2 = new music_vae.MusicVAE("https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/drums_2bar_nade_9_q2");
 //drums_vae_2bar_nade_9_q2.initialize();
 
 /**
@@ -47,19 +45,40 @@ drums_vae_4bar_med_q2 = new music_vae.MusicVAE("https://storage.googleapis.com/m
  * which is better for sampling. Quantized to 2-byte weights.
  */
 drums_4bar_med_lokl_q2 = new music_vae.MusicVAE("https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/drums_4bar_med_lokl_q2");
-drums_4bar_med_lokl_q2.initialize();
 
 /**
  * A medium-sized 2-bar, 9-class onehot drum model with a weak prior (higher KL divergence),
  * which is better for reconstructions and interpolations. Quantized to 2-byte weights.
  */
 drums_4bar_med_q2 = new music_vae.MusicVAE("https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/drums_4bar_med_q2");
-drums_4bar_med_q2.initialize();
 
-console.log("Initializing MusicVAE models...");
+const modelNamesMap = Object.freeze({
+    "music_vae_mel_4bar_q2": music_vae_mel_4bar_q2,
+    "music_vae_2bar_chords": music_vae_2bar_chords,
+    "music_vae_2bar_small": music_vae_2bar_small,
+    "drums_vae_2bar_nade_9_q2": drums_vae_2bar_nade_9_q2,
+    "drums_vae_4bar_med_q2": drums_vae_4bar_med_q2,
+    "drums_4bar_med_lokl_q2": drums_4bar_med_lokl_q2,
+    "drums_4bar_med_q2": drums_4bar_med_q2
+});
+
+console.log("%cInitializing workerVAE.js", `background-color:black;
+                                            color:magenta;padding:0.5em;
+                                            font-weight:bold;`);
 
 self.onmessage = async (e) => {
     switch (e.data.msg) {
+        case "initialize":
+            let models = e.data.models.map( modelName => modelNamesMap[modelName] );
+
+            let f = e.data.chain ? chainInitialization : initOrWaitModels;
+            f(models).then( () => {
+                e.ports[0].postMessage({
+                    msg: "Model initialization completed",
+                    models: e.data.models
+                })
+            });
+            break;
         case "sampleRequest":
             sampleRequest(e);
             break;
@@ -88,7 +107,7 @@ async function interpolationRequest(e){
     } else {
         model = music_vae_mel_4bar_q2;
     }
-    await waitModelInitialization(model);
+    await initOrWaitModels(model);
     let interpolated = await model.interpolate(e.data.seqs, e.data.n, e.data.temperature);
     e.ports[0].postMessage({
         type: "interpolationReply",
@@ -126,7 +145,7 @@ async function sampleRequest(e){
         default:
             throw new Error(`wrong "type" on message for workerVAE.js: ${data}`);
     }
-    await waitModelInitialization(model);
+    await initOrWaitModels(model);
     let data = e.data;
     sequences = await model.sample(e.data.numSamples, data.temperature, data.controlArgs,
                                    data.stepsPerQuarter, data.qpm );
@@ -143,8 +162,8 @@ async function sampleRequest(e){
  */
 async function similarSequencesRequest(e){
     let data = e.data;
-    await waitModelInitialization(music_vae_mel_4bar_q2);
-    let s = await music_vae_mel_4bar_q2.similar(data.seq, data.numSamples, 
+    await initOrWaitModels(music_vae_mel_4bar_q2);
+    let s = await music_vae_mel_4bar_q2.similar(data.seq, data.numSamples,
                                                 data.similarity, data.temperature);
     e.ports[0].postMessage({
         type: "similarSequencesReply",
