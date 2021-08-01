4a8ec8d482bb9648d6a560089d1edefee5b39c9f
HenriAugusto <msndohenri@hotmail.com>
2021-08-01 03:45:03 -0300

    Adapt workerMidiMe.js to use new model initialization system

--- core/core.js
+++ core/core.js
@@ -1,6 +1,6 @@
 async function initialize(){
     initGUI();
-    
+
     PLAYER_HAND.addCards( [
         new SequenceCard(BEETH_9TH ,"Beethoven", "Melody"),
         new SequenceCard(DRUM_SEQ_1 ,"Drums", "Drums"),
@@ -14,7 +14,7 @@ async function initialize(){
         new DrumsGenerator(DRUM_SEQ_3, "Drum Generator 3"),
         new DrumsGenerator(DRUM_SEQ_3, "Drum Generator 3")
     ] );
-    
+
 
     /* The first models we are gonna need are the ones that
      * are capable of generating melodies. To speed up initialization
@@ -26,6 +26,7 @@ async function initialize(){
         MAIN_DECK.addCardsToTop(new SequenceCard(seq, "Deck", "Melody"))
     });
     await RNN.initializeWorker(true);
-    
+    await VAE_MidiMe.initializeWorker(true);
+
     //VAE_MidiMe.initializeWorker();
 }
