1173d684dd516688d5cc9303f2a37d2bf4403aa2
HenriAugusto <msndohenri@hotmail.com>
2021-08-01 13:25:57 -0300

    Generate some cards between VAE and RNN model initialization

--- core/core.js
+++ core/core.js
@@ -1,5 +1,6 @@
-function initialize(){
+async function initialize(){
     initGUI();
+    
     PLAYER_HAND.addCards( [
         new SequenceCard(BEETH_9TH ,"Beethoven", "Melody"),
         new SequenceCard(DRUM_SEQ_1 ,"Drums", "Drums"),
@@ -13,7 +14,18 @@ function initialize(){
         new DrumsGenerator(DRUM_SEQ_3, "Drum Generator 3"),
         new DrumsGenerator(DRUM_SEQ_3, "Drum Generator 3")
     ] );
-    VAE.initializeWorker();
-    RNN.initializeWorker();
-    VAE_MidiMe.initializeWorker();
+    
+
+    /* The first models we are gonna need are the ones that
+     * are capable of generating melodies. To speed up initialization
+     * we initialize them first and only then initialize the rest */
+    await VAE.initializeWorker();
+    let seqs = await VAE.requestSamples("melody", 4, 10);
+    seqs.forEach( seq => {
+        console.log("card generated");
+        MAIN_DECK.addCardsToTop(new SequenceCard(seq, "Deck", "Melody"))
+    });
+    await RNN.initializeWorker(true);
+    
+    //VAE_MidiMe.initializeWorker();
 }
