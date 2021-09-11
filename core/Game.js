d21205c632c727c19d21dc1c8de33d78cee2b31e
HenriAugusto <msndohenri@hotmail.com>
2021-09-10 22:01:13 -0300

    Implement basic save/load functionality for Game class

--- core/Game.js
+++ core/Game.js
@@ -1,4 +1,27 @@
 class Game {
     static maxHandSize = 10;
     static deckSize = 60;
+
+    /**
+    * Creates an snapshot containing all the information needed
+    * to recreate this object later. Meant to be used with {@link SaveLoad}.
+    * @returns {Object} snapshot
+    */
+    static save(){
+        return {
+            maxHandSize: Game.maxHandSize,
+            deckSize: Game.deckSize
+        }
+    }
+
+    /**
+     * Reconstructs a object from it snapshot. Meant to be used with {@link SaveLoad}.
+     * @static
+     * @param {Object} obj - As returned from the {@link save()} method.
+     * @returns
+     */
+    static load(obj){
+        Game.maxHandSize = obj.maxHandSize;
+        Game.deckSize = obj.deckSize;
+    }
 }
