13cc9ab1a0dfd05dd27061551a2f63fe85ced20b
HenriAugusto <msndohenri@hotmail.com>
2021-09-11 15:08:08 -0300

    Do not add pre-defined cards (for testing) on startup

--- core/core.js
+++ core/core.js
@@ -3,22 +3,6 @@ import PianoEditor from '../SequenceEditors/PianoEditor/PianoEditor.js'
 async function initialize(){
     initGUI();
 
-    PLAYER_HAND.addCards( [
-        new EditorCard(),
-        new ContinueCard(),
-        new SequenceCard(BEETH_9TH ,"Beethoven", "Melody"),
-        new SequenceCard(DRUM_SEQ_1 ,"Drums", "Drums"),
-        new MelodyGenerator(MELODY_1, "Melody Generator"),
-        new MelodyGenerator(BASS_1, "Melody Generator"),
-        new MelodyGenerator(MELODY_2, "Melody Generator"),
-        new MelodyGenerator(MELODY_3, "Melody Generator"),
-        new MelodyGenerator(MELODY_4, "Melody Generator"),
-        new DrumsGenerator(DRUM_SEQ_1, "Drum Generator 1"),
-        new DrumsGenerator(DRUM_SEQ_2, "Drum Generator 2"),
-        new DrumsGenerator(DRUM_SEQ_3, "Drum Generator 3"),
-        new DrumsGenerator(DRUM_SEQ_3, "Drum Generator 3")
-    ] );
-
     EditorCard.pianoEditor = new PianoEditor();
 
     /* The first models we are gonna need are the ones that
